e07b77dec40041e0b9d203a80745490e49e21330
Felipe Mareca <felipemarecasb@gmail.com>
2025-12-09 19:01:30 -0300

    components: Logo, navbar, countdown timer, form

---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,11 @@
+import styles from './styles.module.css';
+
+export function Logo() {
+  return (
+    <div className={styles.logo}>
+      <a className='' href='#'>
+        <img src='src\assets\media\logo-dark.png'></img>
+      </a>
+    </div>
+  );
+}
