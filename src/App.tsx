b9aa26f962afdb0aa2dda48dcddf86842b03d117
Felipe Mareca <felipemarecasb@gmail.com>
2025-12-09 14:39:26 -0300

    learning about props

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,12 @@ import { Heading } from './components/Heading';
 export function App() {
   return (
     <>
-      <Heading />
+      <Heading attr={123} attr2='String'>
+        Olá Mundo!
+      </Heading>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, deserunt. Officiis architecto vel nesciunt quaerat eaque, voluptatibus, explicabo illo, accusantium alias consectetur vero deserunt dignissimos harum minima repellat quod? Accusamus?
+    </p>
     </>
   );
 }
--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,7 +1,7 @@
 import styles from './Heading.module.css';
 
-export function Heading() {
+export function Heading(props) {
   //  const classes = `${styles.heading} ${styles.orange}`;
   //  return <h1 className=`${styles.heading} ${styles.orange}`>Heading Component</h1>;
-  return <h1 className={styles.heading}>Heading Component</h1>;
+  return <h1 className={styles.heading}>{props.children}</h1>;
 }
