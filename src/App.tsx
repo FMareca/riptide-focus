027400bfc63523e776346b99ec2fa1fac3c69e27
Felipe Mareca <felipemarecasb@gmail.com>
2025-12-11 16:37:05 -0300

    NotFound and AboutPomodoro Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 // CamelCase
+// import { AboutPomodoro } from './pages/AboutPomodoro';
 import { Home } from './pages/Home';
+// import { NotFound } from './pages/NotFound';
 
 import './styles/global.css';
 import './styles/theme.css';
