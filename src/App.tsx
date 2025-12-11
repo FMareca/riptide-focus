a100b476bf9b4ca34d0a313be334e680a4b1fc11
Felipe Mareca <felipemarecasb@gmail.com>
2025-12-11 15:27:48 -0300

    using page template

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,9 @@
 // CamelCase
-import './styles/theme.css';
-import './styles/global.css';
+import { Home } from './pages/Home';
 
-import { Container } from './components/Container';
-import { Logo } from './components/Logo';
-import { Menu } from './components/Menu';
-import { CountDown } from './components/CountDown';
-import { Footer } from './components/Footer';
-import { MainForm } from './components/MainForm';
+import './styles/global.css';
+import './styles/theme.css';
 
 export function App() {
-  return (
-    <>
-      <Container>
-        <Logo />
-      </Container>
-
-      <Container>
-        <Menu />
-      </Container>
-
-      <Container>
-        <CountDown />
-      </Container>
-
-      <Container>
-        <MainForm />
-      </Container>
-
-      <Container>
-        <Footer />
-      </Container>
-    </>
-  );
+  return <Home />;
 }
